e32547379aa54385216808762cad518d37b8d1c5
swapnil <pkswalke@gmail.com>
2017-09-10 21:02:41 +0530

    selenium Test

--- src/test/java/com/demo/selenium/SeleniumDemo.java
+++ src/test/java/com/demo/selenium/SeleniumDemo.java
@@ -1,10 +1,28 @@
 package com.demo.selenium;
 
-import org.testng.annotations.Test;
+//package example;		
 
-public class SeleniumDemo {
-	@Test
-	public void PrintSel() {
-		System.out.println("Testtest..test.");
-	}
-}
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class SeleniumDemo {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/selenium/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
